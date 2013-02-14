4d04099ccb496c99aaa8526ece5142c69c89bed2
Antonio Marques <acmarques@gmail.com>
2013-02-14 18:25:16 -0200

    making 'output_log_file' configurable

--- plugins/common/logger.py
+++ plugins/common/logger.py
@@ -1,7 +1,9 @@
-import logging
+import logging, yaml
 
+
+config = yaml.load(open('config/snortsam_parsible.yml')) 
 logging.basicConfig(level=logging.INFO)
-h = logging.FileHandler('log/snortsam_parsible.log')
+h = logging.FileHandler(config["output_log_file"])
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 h.setFormatter(formatter)
 logger = logging.getLogger('snortsam_parsible')
